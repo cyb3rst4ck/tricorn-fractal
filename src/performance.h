297d797bf517e6eb4e26fe05dd87569493834737
cyb3rst4ck <64363978+cyb3rst4ck@users.noreply.github.com>
2023-07-25 14:55:17 +0200

    Update performance.h

--- src/performance.h
+++ src/performance.h
@@ -2,5 +2,6 @@
 #include <time.h>
 #include <complex.h>
 
+// Function prototype collection so that the compiler knows how to call the respective functions with their arguments
 typedef void (*func_ptr)(complex float,  size_t,  size_t,  float,  unsigned int,  unsigned char *);
 void performanceFractal(func_ptr algo, float complex start, size_t width, size_t height, float res, unsigned n, unsigned char* img, int reruns);
