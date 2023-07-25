0f0d5c9d602332fa28bab10a797af2878a9a65ab
cyb3rst4ck <64363978+cyb3rst4ck@users.noreply.github.com>
2023-07-25 14:54:05 +0200

    Update bitmap.h

--- src/bitmap.h
+++ src/bitmap.h
@@ -1,2 +1,3 @@
+// Function prototype collection so that the compiler knows how to call the respective functions with their arguments
 void generateBmp(unsigned char* img, char* filename, size_t width, size_t height, size_t size);
 
