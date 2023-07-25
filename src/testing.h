5ff3148b43ea04e1da638068aaa3d3d1c44e0776
cyb3rst4ck <64363978+cyb3rst4ck@users.noreply.github.com>
2023-07-25 14:56:32 +0200

    Update testing.h

--- src/testing.h
+++ src/testing.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// Function prototype collection so that the compiler knows how to call the respective functions with their arguments
 void firstTestCompareArrays(unsigned char* imgArrayBitmap, size_t size);
 
 void callDesiredTestcase(unsigned char* imgArrayBitmap, size_t size, int testcase);
