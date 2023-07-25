0513f6e9598a3fb8ac45364363961af06f980aa8
cyb3rst4ck <64363978+cyb3rst4ck@users.noreply.github.com>
2023-07-25 14:54:46 +0200

    Update fractal.h

--- src/fractal.h
+++ src/fractal.h
@@ -1,3 +1,4 @@
+// Function prototype collection so that the compiler knows how to call the respective functions with their arguments
 typedef void (*func_ptr)(complex float,  size_t,  size_t,  float,  unsigned int,  unsigned char *);
 
 func_ptr selectAlgorithm(int implementation);
